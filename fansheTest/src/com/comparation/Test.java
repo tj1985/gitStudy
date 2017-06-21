26179135a308f81ab751fb6d03d4ed042204e7c9
tj <tj_1985_gillette@126.com>
2017-06-21 09:38:26 +0800

    e

--- fansheTest/src/com/comparation/Test.java
+++ fansheTest/src/com/comparation/Test.java
@@ -9,7 +9,7 @@ import java.util.Map;
 public class Test {
 
 	public static void main(String[] args) {
-		int m =8;
+		int m =7;
 		        for (int i = 0; i < m; i++) {
 		            int t =0; //t是用来存放距离边界需要多少空格
 		            if (i >= m / 2) {// >是m为奇数的情况下的判断，=是m为偶数的情况下的判断，这里将过半的那些i值转换为与它对称的i值
--- fansheTest/src/com/fanshe/test/Test.java
+++ fansheTest/src/com/fanshe/test/Test.java
@@ -1,61 +1,28 @@
 package com.fanshe.test;
 
-import java.io.StringWriter;
 import java.util.LinkedList;
-
 
 public class Test {
 	private static final LinkedList<String> tranInfoList = new LinkedList<String>();
-	
+
 	public static void main(String[] args) {
-		
-		tranInfoList.add("1");
-		tranInfoList.add("2");
-		tranInfoList.add("3");
-		
-		System.out.println(tranInfoList.poll());
+		int m = 7;
+		for (int i = 0; i < m; i++) {
+			int t = 0; 					// tæ˜¯ç”¨æ¥å­˜æ”¾è·ç¦»è¾¹ç•Œéœ€è¦å¤šå°‘ç©ºæ ¼
+			if (i >= m / 2) {			// >æ˜¯mä¸ºå¥‡æ•°çš„æƒ…å†µä¸‹çš„åˆ¤æ–­ï¼Œ=æ˜¯mä¸ºå¶æ•°çš„æƒ…å†µä¸‹çš„åˆ¤æ–­ï¼Œè¿™é‡Œå°†è¿‡åŠçš„é‚£äº›iå€¼è½¬æ¢ä¸ºä¸å®ƒå¯¹ç§°çš„iå€¼
+				t = m - 1 - i;
+			} else {
+				t = i;
+			}
+			for (int j = 1; j <= m; j++) {
+				if (j >= (m + 1) / 2 - t && j <= (m + 1) / 2 + t) {
+					System.out.print("*");
+				} else {
+					System.out.print(" ");
+				}
+			}
+			System.out.print("\n");
+		}
 	}
-	
-	/**
-     * Á”ïÅ”²‚«ˆ—.
-     *
-     * @param taxIncludeAmt
-     *            Å‹àŠz
-     * @param taxRate
-     *            Å—¦
-     * @param roudupType
-     *            0:Ø‚èã‚°A1:lÌŒÜ“ü
-     * @return Å”²‚«‹àŠz
-     */
-    public static double ExcludeTax(double taxIncludeAmt, double taxRate, int roudupType) {
-        double taxExcludeAmt = 0;
-
-        if (taxIncludeAmt < 0) {
-            if (roudupType == 0) {
-                // Ø‚èã‚°
-                taxExcludeAmt = Math.ceil(-1.0 * taxIncludeAmt * 100 / (100 + taxRate));
-            } else {
-                // lÌŒÜ“ü
-                taxExcludeAmt = Math.round(-1.0 * taxIncludeAmt * 100 / (100 + taxRate));
-            }
-            taxExcludeAmt = -1 * taxExcludeAmt;
-        } else {
-            if (roudupType == 0) {
-                // Ø‚èã‚°
-                taxExcludeAmt = Math.ceil(taxIncludeAmt * 100 / (100 + taxRate));
-            } else {
-                // lÌŒÜ“ü
-                taxExcludeAmt = Math.round(taxIncludeAmt * 100 / (100 + taxRate));
-            }
-        }
-
-        return taxExcludeAmt;
-    }
-    
-    public static double exec(double taxIncludeAmt, double taxRate, int roudupType) {
-    	double taxExcludeAmt = 0;
-    	taxExcludeAmt = Math.round(taxIncludeAmt - taxIncludeAmt * taxRate/100);
-    	return taxExcludeAmt;
-    }
 
 }
